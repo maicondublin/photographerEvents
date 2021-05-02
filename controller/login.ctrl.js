ff20b04f2eba3a3a4f3da76abe5b782471046efc
maicondublin <maicondublin@gmail.com>
2021-05-02 10:37:33 +0000

    updates

--- controller/login.ctrl.js
+++ controller/login.ctrl.js
@@ -1,18 +1,19 @@
 // const user = require('../models/user')
-const user = require('../controller/user.ctrl')
+const userController = require('../controller/user.ctrl')
 const bcrypt = require('bcrypt')
 
 const login = async function( data ) {
 
     const{name, password} = data
+
     const user = await userController.loadByName({name})
 
     if(!user) throw new Error('User not found!')
     if(!await bcrypt.compare(password, user.password)) throw new Error('Invalid password!')
 
-    user.token = await bcrypt.hash(user._id, 10)
+    user.token = await bcrypt.hash(`{id:${user.id}, ${Date()}}`, 10)
 
-    await user.update(user)
+    await userController.update(user)
 
     return user.token
 
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,16 @@
+const userController = require('../controller/user.ctrl')
+
 //inherit params from routes where its implemented 
 const validate = async function(req, res, next) {
-    const validate = async function(req, res, next) {
-        const user = await userController.loadByToken(token)
-    
+
+     const [bearer, token] =  req.headers.token.split('')
+
+     const user = await userController.loadByToken(token)
+
     //NEXT => used to move forward to a NEXT route/path.
     if(user) return next()
     //401 => used to 'warm'anuauthorized access.
     res.status(401).send('User not authenticated!')
-}
 }
 
 module.exports = validate

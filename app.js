d81c4e8fdba06334d3685692ccde8b4d9c5b2708
maicondublin <maicondublin@gmail.com>
2021-05-03 01:29:00 +0000

    update cors

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-// const bodyParcer = require('body-parcer')
- const bodyParcer = require('body-parser')
+const cors = require('cors')
+const bodyParcer = require('body-parser')
 const events = require('./routes/events')
 const user = require('./routes/user')
 const login = require('./routes/login')
 const app = express()
 
+app.use(cors())
 app.use(bodyParcer.json())
 app.use(bodyParcer.urlencoded({extended:false}))
 app.use(events)
 app.use(user)
 app.use(login)
 
-
 app.listen(process.env.PORT || 3001, function(){
     console.log('Listining to the port '+ (process.env.PORT || '3001'))
 })
-
 
 module.exports = app
